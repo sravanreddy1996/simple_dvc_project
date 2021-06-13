fcb8c54c2be4fb49ac1d277241d4bc89c1fdb154
shravan.k <shravan.k@appnomic.com>
2021-06-13 18:09:53 +0530

    notebooks updated and schema updated

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,15 @@
+import pytest
+
+class NotInRangeError(Exception):
+    def __init__(self, message="value not in range"):
+        self.message_ = message
+        super().__init__(self.message_)
+
 
 def test_generic():
-    a =2
-    b = 2
-    assert a == b
+    a = 11
+    # with pytest.raises(NotInRangeError):
+    if a not in range(10, 20):
+        raise NotInRangeError
+    else:
+        assert True
